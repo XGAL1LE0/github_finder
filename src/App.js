1b989ce4fa9d2f9be2a6337fc49a151cf4a821fa
XGAL1LE0 <x@gal1le0.io>
2020-05-23 01:11:58 +0800

    Added axios to fetch data from github api. Added async await componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import Useritem from './components/users/Useritem';
+import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users');
+
+    console.log(res.data);
+  }
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
-        <Useritem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
